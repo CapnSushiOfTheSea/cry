0435fa0c058656918b8604c426236bd9ed75a809
Noodle Deluxe <76448871+NoodleDX@users.noreply.github.com>
2023-08-18 10:43:11 -0600

    Update cry.py

--- cry.py
+++ cry.py
@@ -1,10 +1,22 @@
 from time import sleep
-from random import randint
+from random import randint, choice
+
+userInput = input('What to cry about? (Leaving it blank will pick a random choice) ')
+
+thingsToCryAbout = [
+    'taxes',
+    'debt',
+    'i made the npc sad',
+    'idk what im crying about but wahhh'
+]
 
 def cryAbout(thing):
+    if thing == '':
+        thing = choice(thingsToCryAbout)
     thingYelled = thing.upper()
     tears = 1
     print(f'WAAHHHH WAHHHH!! {thingYelled}!!!!! (1 tear)')
+    sleep(.5)
     try:
         crying = True
         while crying:
@@ -19,4 +31,4 @@ def cryAbout(thing):
     except KeyboardInterrupt:
         print('No')
 
-cryAbout('something')
+cryAbout(userInput)
